253b87618234563447260bbea97ad3f47c6fc9e4
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-19 22:34:02 -0300

    Tables being created with success

--- person.api/src/main/java/br/com/leomanzini/person/api/entity/Phone.java
+++ person.api/src/main/java/br/com/leomanzini/person/api/entity/Phone.java
@@ -1,5 +1,14 @@
 package br.com.leomanzini.person.api.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import br.com.leomanzini.person.api.enums.PhoneType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,9 +19,18 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "phones")
 public class Phone {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private PhoneType type;
+	
+	@Column(nullable = false)
 	private String number;
 }
